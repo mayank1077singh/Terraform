b385f66e9c4c3e58acb4a07c9ca4e685f9e42941
mayank1077singh <mayank1077singh@gmail.com>
2023-01-26 23:38:43 +0530

    added ec2 instance

--- main.tf
+++ main.tf
@@ -8,3 +8,12 @@ terraform {
 resource "aws_s3_bucket" "sample_bucket" {
   bucket = "elasticbeanstalk-us-east-1-105608008969"
 }
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
