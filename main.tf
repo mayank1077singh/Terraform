b73e140a5aa62cd5910e71c026d05ce5150698a3
Mandarin <33388292+mayank1077singh@users.noreply.github.com>
2023-07-27 18:20:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,3 @@
-terraform {
-  backend "s3" {
-    bucket         = "mayank-terraform-statefile"
-    dynamodb_table = "terraform-state-lock-dynamo"
-    key            = "Statefile"
-    region         = "ap-south-1"
-    encrypt        = true
-  }
-}
-resource "aws_s3_bucket" "sample_bucket" {
-  bucket = "elasticbeanstalk-us-east-1-105608008969"
-}
 
 resource "aws_instance" "web" {
   ami           = "ami-0f2e14a2494a72db9"
