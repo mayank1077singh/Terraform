f7566e2fef1fbe34cb1da6d62bbc4e646bdd0fdb
Mandarin <33388292+mayank1077singh@users.noreply.github.com>
2023-07-31 20:37:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,8 @@
-
-resource "aws_instance" "web" {
-  ami           = "ami-0f2e14a2494a72db9"
-  instance_type = "t3.micro"
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket"
 
   tags = {
-    Name = "Hello"
+    Name        = "My bucket"
+    Environment = "Dev"
   }
 }
